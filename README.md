01c9d33944ec1771ea7fd669e6d35aa2eabc8851
= <=>
2020-08-25 12:08:43 -0300

    readme file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# battleship-backend
+## Batalha naval
 
-Back End for a battle ship game created in JAVA
+Back End de jogo de batalha naval multi-player criado para aprendizado de Socket.io e ReactJS.
+
+- Back End Rest => Java Spring Boot
+- Front End => ReactJS [Link do Projeto](https://github.com/diogorolins/new-battleship-frontend)
+- Back End Socket => NodeJS [Link do Projeto](https://github.com/diogorolins/new-battleship-socket)
+
+[Jogar - Link do Jogo](http://batalhanaval.diogorolins.com.br)
--- src/main/java/com/diogorolins/battleShip/config/Instantiation.java
+++ src/main/java/com/diogorolins/battleShip/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.diogorolins.battleShip.config;
 
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,14 +9,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.diogorolins.battleShip.model.Game;
 import com.diogorolins.battleShip.model.Player;
+import com.diogorolins.battleShip.model.Ship;
 import com.diogorolins.battleShip.model.ShipType;
+import com.diogorolins.battleShip.repositories.GameRepository;
 import com.diogorolins.battleShip.repositories.PlayerRepository;
+import com.diogorolins.battleShip.repositories.ShipRepository;
 import com.diogorolins.battleShip.repositories.ShipTypeRepository;
 
 
 @Configuration
-@Profile("prod")
+@Profile("dev")
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
@@ -32,20 +37,20 @@ public class Instantiation implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		ShipType shipType1 = new ShipType(null, "Porta-Aviões", 5);
+		//ShipType shipType1 = new ShipType(null, "Porta-Aviões", 5);
 		//ShipType shipType2 = new ShipType(null, "Porta-Aviões 2", 5);
 		ShipType shipType3 = new ShipType(null, "Navio-Tanque", 4);
 		//ShipType shipType4 = new ShipType(null, "Navio-Tanque 2", 4);
-		ShipType shipType5 = new ShipType(null, "Contra-Torpedeiro", 3);
+		//ShipType shipType5 = new ShipType(null, "Contra-Torpedeiro", 3);
 		//ShipType shipType6 = new ShipType(null, "Contra-Torpedeiro 2", 3);
 		ShipType shipType7 = new ShipType(null, "Submarino", 2);
 		//ShipType shipType8 = new ShipType(null, "Submarino 2", 2);
 
-		//shipTyperepository.saveAll(
-		//		Arrays.asList(shipType1, shipType2, shipType3, shipType4, shipType5, shipType6, shipType7, shipType8));
-
 		shipTyperepository.saveAll(
-					Arrays.asList(shipType1, shipType3, shipType5, shipType7));
+				Arrays.asList(shipType3,shipType7));
+
+		//shipTyperepository.saveAll(
+		//			Arrays.asList(shipType1, shipType3, shipType5, shipType7));
 		
 				
 		Player player1 = new Player(null, "Diogo", "diogo@email.com", pe.encode("123456"),null, true);
@@ -55,6 +60,9 @@ public class Instantiation implements CommandLineRunner {
 
 		playerRepository.saveAll(Arrays.asList(player1, player2, player3, player4));
 		
-	
+		
+		
+	//	Game game = new Game(null, null, null, null, null);
+	//	gameRepository.save(game);
 	}
 }
